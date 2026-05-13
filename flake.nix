4a4bbbbf4d78f2800ce20f7e367313727ae61af9
Jb <42501418+MachXNU@users.noreply.github.com>
2026-05-13 19:11:03 +0200

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "gdiextract";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "gdiextract";
+          version = "1.0.0";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+          ];
+
+          buildInputs = with pkgs; [
+            argparse
+          ];
+
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+          ];
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/gdiextract";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            ninja
+            gcc
+            gdb
+            clang-tools
+            argparse
+          ];
+        };
+      });
+}
