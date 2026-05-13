66d33411254fddf7f9842e5a2901e6b7b7e0fa88
MachXNU <42501418+MachXNU@users.noreply.github.com>
2026-05-13 22:23:01 +0200

    FIx build on Nix and non-Nix systems

--- flake.nix
+++ flake.nix
@@ -9,9 +9,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
       in {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "gdiextract";
